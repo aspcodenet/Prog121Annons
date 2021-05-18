7669534cdf93c1eec6ed37e0f5a43905e2bb8f38
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-18 10:44:39 +0200

    Fixes

--- Prog121Annons/Annons.cs
+++ Prog121Annons/Annons.cs
@@ -6,10 +6,26 @@ namespace Prog121Annons
 {
     public class Annons
     {
+
         public string Rubrik { get; set; }
         public decimal Pris { get; set; }
         public string Username { get; set; }
         public DateTime SlutDatumOchTid{ get; set; }
+
+        public bool SlutarSnart()
+        {
+            var omTolvTimmar = DateTime.Now;
+            omTolvTimmar = omTolvTimmar.AddHours(12);
+            if (SlutDatumOchTid > omTolvTimmar)
+                return false;
+            return true;
+            //SLutDatumTid < Datetime.now + 12 timmar
+        }
+
+        public string GetWeekday()
+        {
+            return SlutDatumOchTid.DayOfWeek.ToString();
+        }
 
         public DateTime Created { get; set; }
 
--- Prog121Annons/AnnonsSystem.cs
+++ Prog121Annons/AnnonsSystem.cs
@@ -26,6 +26,32 @@ namespace Prog121Annons
             File.WriteAllText(@"C:\Users\stefa\Temp\annonser.txt", json);
         }
 
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. Skapa ny");
+            Console.WriteLine("2. Lista");
+            Console.WriteLine("0. Avsluta");
+        }
+
+        public void SkrivUtAnnonser(List<Annons> annonsList)
+        {
+            Console.Clear();
+            Console.WriteLine("---- ALLA ANNONSER ----");
+            foreach (var annons in annonsList)
+            {
+                string dag = annons.GetWeekday();
+                Console.WriteLine($"{annons.Rubrik} - {annons.Pris}");
+                if (annons.SlutarSnart())
+                {
+                    Console.WriteLine("***Slutar snart***");
+                }
+                Console.WriteLine($"{annons.Description}");
+                Console.WriteLine($"{dag}, {annons.SlutDatumOchTid}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
         public void Run()
         {
             var listaMedAnnonser = GetAnnonserFromFile();
@@ -34,7 +60,22 @@ namespace Prog121Annons
 
             while (true)
             {
-                //1. 
+                ShowMenu();
+                Console.WriteLine("Ange vad du vill göra:");
+                int sel = Convert.ToInt32(Console.ReadLine());
+                if (sel == 1)
+                {
+                    //Create
+                }
+
+                if (sel == 2)
+                {
+                    SkrivUtAnnonser(listaMedAnnonser);
+                }
+                else if (sel == 0)
+                {
+                    break;
+                }
             }
 
 
